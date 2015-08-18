03fad24dc74ed6a140e037975cf5e2e8c7e08e26
Aixtuz <aixtuz@gmail.com>
2015-08-18 11:26:43 +0800

    CollectionItem无限循环滚动
    1. 设置 Cell
        - 尺寸, 间距, 浮动, 分页, 滚动条
    2. 无限循环滑动 Item
        - 计算索引: index = (item - 1 + imgIndex + 4) % 4
        - 设置数据: headLines[count(4)]
    3. 监听滑动减速
        - 回归复用 Cell: 0 - 1, 左0右2, 减 1 得 -/+1, 做变化增量,

--- NeteaseNews/BannerLoopController.m
+++ NeteaseNews/BannerLoopController.m
@@ -14,6 +14,10 @@
 
 // 模型数组
 @property (nonatomic, strong) NSArray *headLines;
+// 布局属性
+@property (weak, nonatomic) IBOutlet UICollectionViewFlowLayout *flowLayout;
+// 当前图片索引
+@property (nonatomic, assign) NSInteger imgIndex;
 
 @end
 
@@ -28,12 +32,26 @@ static NSString * const reuseIdentifier = @"headline";
     [KCLHeadLine headLines:^(NSArray *array) {
         
         // 测试: headLines: 方法的回调数据
-        NSLog(@"%@", array);
+        NSLog(@"\nBannerLoop数据:\n%@", array);
         
         // 属性存储数据(Setter)
         self.headLines = array;
         //!!! 异步请求, 获取数据, 此前已执行数据源方法(无数据)
     }];
+    
+    // 设置 Cell 样式
+    // 尺寸
+    self.flowLayout.itemSize = self.collectionView.bounds.size;
+    // 横间距
+    self.flowLayout.minimumInteritemSpacing = 0;
+    // 纵间距
+    self.flowLayout.minimumLineSpacing = 0;
+    // H: 横向浮动, V: 纵向浮动
+    self.flowLayout.scrollDirection = UICollectionViewScrollDirectionHorizontal;
+    // 分页
+    self.collectionView.pagingEnabled = YES;
+    // 滚动条
+    self.collectionView.showsHorizontalScrollIndicator = NO;
 }
 
 // 重写 setter 方法
@@ -45,28 +63,72 @@ static NSString * const reuseIdentifier = @"headline";
     // 异步请求, 获取数据, 属性存储后, 刷新数据
     [self.collectionView reloadData];
     
+    // indexPath.item: 0 - 1, 加载第二个 Cell, 显示第一张 img
+    NSIndexPath *indexPath = [NSIndexPath indexPathForItem:1 inSection:0];
+    [self.collectionView scrollToItemAtIndexPath:indexPath atScrollPosition:0 animated:NO];
 }
 
 #pragma mark - <UICollectionViewDataSource>
 
+// 返回 Cell 数
 - (NSInteger)collectionView:(UICollectionView *)collectionView numberOfItemsInSection:(NSInteger)section {
 
-    // items count
     return self.headLines.count;
 }
 
+// 返回 Cell
 - (UICollectionViewCell *)collectionView:(UICollectionView *)collectionView cellForItemAtIndexPath:(NSIndexPath *)indexPath {
     
     // 实例化 自定义 Cell
     KCLHeadLineCell *cell = [collectionView dequeueReusableCellWithReuseIdentifier:reuseIdentifier forIndexPath:indexPath];
+    /*
+     0. 初始 item = 1
+     
+     1. currentItem = 1, nextItem = 2, imgIndex = 0:
+        currentData : item(1) - 1 + imgIndex(0) => (0 + count(4)) % count(4) = 0
+        nextData    : item(2) - 1 + imgIndex(0) => (1 + count(4)) % count(4) = 1
+     
+     2. currentItem = 2, nextItem = 3, imgIndex = 1:
+        currentData : item(2) - 1 + imgIndex(1) => (2 + count(4)) % count(4) = 2
+        nextData    : item(3) - 1 + imgIndex(1) => (3 + count(4)) % count(4) = 3
+     
+     // 循环, 无限滚动
+     3. currentItem = 3, nextItem = 0, imgIndex = 2:
+        currentData : item(3) - 1 + imgIndex(2) => (4 + count(4)) % count(4) = 0
+        nextData    : item(0) - 1 + imgIndex(2) => (1 + count(4)) % count(4) = 1
+     */
+    // Cell: N 显示 Data: N - 1
+    NSInteger index = (indexPath.item - 1 + self.imgIndex + self.headLines.count) % self.headLines.count;
     
     // 保存 Cell 序号
-    cell.tag = indexPath.item;
+    // cell.tag = indexPath.item;
+    cell.tag = index;
     // 传递 Cell 模型数据
-    cell.headLine = self.headLines[indexPath.item];
+    // cell.headLine = self.headLines[indexPath.item];
+    cell.headLine = self.headLines[index];
     
     return cell;
 }
+
+// CollectionView 监听滚动减速
+-(void)scrollViewDidEndDecelerating:(UIScrollView *)scrollView {
+    
+    /*
+     滑动完毕, 回归复用 Cell: 0 - 1,
+     左: 0 - 0, offset: 0 * width;
+     右: 0 - 2, offset: 2 * width;
+     // -1, 得 -/+1, 左-右+;
+     */
+    int offset = scrollView.contentOffset.x / scrollView.bounds.size.width - 1;
+    
+    // 计算 imgIndex, 根据滚动方法 +/-1, 取余循环
+    self.imgIndex = (self.imgIndex + offset + self.headLines.count) % self.headLines.count;
+    
+    // 滑动完毕, 立刻 回到 第二个 Cell
+    NSIndexPath *indexPath = [NSIndexPath indexPathForItem:1 inSection:0];
+    [self.collectionView scrollToItemAtIndexPath:indexPath atScrollPosition:0 animated:NO];
+}
+
 
 #pragma mark - <UICollectionViewDelegate>
 
